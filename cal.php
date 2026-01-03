e68c48264d3e12f3c7c27651ba74d67c1e80aa76
devp01 <devi_pisharady@yahoo.com>
2026-01-03 12:39:53 +0000

    code update

--- cal.php
+++ cal.php
@@ -1,15 +1,7 @@
 <?php
-
-use PHPUnit\Framework\TestCase;
-use App\Calculator;
-
-class CalculatorTest extends TestCase
-{
-    public function testAdd()
-    {
-        $calculator = new Calculator();
-        $result = $calculator->add(1, 2);
-        $this->assertEquals(3, $result);
+class Calculator {
+    public function add($a, $b) {
+        return $a + $b;
     }
 }
 
